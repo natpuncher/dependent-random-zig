7e24818dfdc80a4f34cf41cb9455f545f8aa3510
np <nat.puncher@gmail.com>
2026-02-24 19:00:10 +0200

    migrate to zig 0.15

--- build.zig
+++ build.zig
@@ -4,14 +4,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("dependent_random_zig", .{
-        .root_source_file = b.path("dependentrandom.zig"),
-    });
-
-    const tests = b.addTest(.{
+    const dependent_random_module = b.addModule("dependent_random_zig", .{
         .root_source_file = b.path("dependentrandom.zig"),
         .target = target,
         .optimize = optimize,
+    });
+
+    const tests = b.addTest(.{
+        .root_module = dependent_random_module,
     });
 
     const test_step = b.step("test", "Run unit tests");
